6b81da30de3f1064649c3b8483718efa8437bdc1
Gabriel Lemos Rodrigues <glemosro@uwo.ca>
2022-11-28 17:06:46 -0500

    functioning hide button

--- react-frontend/src/LoginComponent.js
+++ react-frontend/src/LoginComponent.js
@@ -1,22 +1,31 @@
 import "./Login.css"
+import React from "react"
 
 const LoginComponent = () => {
 
-    // const [popupStyle, showPopup] = useState("hide");
+    const [showRegister, setShow] = React.useState(false);
 
-    // const popup = () => {
-    //     alert()
-    // }
+    const handleShow = () => {
+        setShow(showRegister => !showRegister);
+    }
+
+    let toggleLoginCheck = showRegister ? "null" : "";
     return ( 
         <div className="loginCover">
-            <h1>Movie Booking System</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-            
-            <p className="text">Or login using</p>
-            <div className="login-btn">Login</div>
-            <div className="alt-login">
-                <div className="login-btn" id="forgot">Forgot My Password</div>
+            <div className={`loginPage${toggleLoginCheck}`}>
+                <h1>Movie Booking System</h1>
+                <input type="text" placeholder="username" />
+                <input type="password" placeholder="password" />
+                
+                <p className="text">Or login using</p>
+                <div className="login-btn">Login</div>
+                <div className="alt-login">
+                    <div className="login-btn" id="forgot">Forgot Password</div>
+                    <div className="login-btn" id="register" onClick= {() => {handleShow();}}>Register New User</div>
+                </div>
+            </div>
+            <div className="registerNewUser">
+                <h1>Hello there, this is register</h1>
             </div>
         
         </div>
