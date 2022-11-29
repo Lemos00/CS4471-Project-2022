6f569926ef15451ed967488e509a4b9b3721aa29
Gabriel Lemos Rodrigues <glemosro@uwo.ca>
2022-11-28 20:35:49 -0500

    create components folder

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import LoginComponent from './LoginComponent/LoginComponent';
-import LoginPage from './LoginPage/LoginPage';
+import LoginPage from './Components/LoginPage/LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
