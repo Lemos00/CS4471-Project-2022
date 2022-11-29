4b31e80268cacaf1ff7d078a52bdbfd433d91731
Gabriel Lemos Rodrigues <glemosro@uwo.ca>
2022-11-29 18:23:40 -0500

    craete staff page template

---
+++ react-frontend/src/Components/StaffPage/StaffPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./StaffPage.css"
+
+// MAIN COMPONENT, LOGIN PAGE MAIN
+const StaffPage = () => {
+
+    // const [showRegister, setShowRegister] = React.useState(false);
+
+
+    return (
+        <div className="loginCover">
+            
+            <h1>Hello there</h1>
+        
+        </div>
+     );
+}
+ 
+export default StaffPage;
