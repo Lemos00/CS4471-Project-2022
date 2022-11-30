ae2fa7ee86c3da5854942a51a997c273164a7e84
Gabriel Lemos Rodrigues <glemosro@uwo.ca>
2022-11-29 23:35:42 -0500

    fix minor issues

--- react-frontend/src/Components/LoginComponent/LoginComponent.js
+++ react-frontend/src/Components/LoginComponent/LoginComponent.js
@@ -11,18 +11,18 @@ const LoginComponent = ({setShowRegister}) => {
     return ( 
         <div className="loginCover">
             <form action="" method="post">
-            <div className="loginPage">
-                <h1>Book-A-Movie</h1>
-                <input className="loginInput" type="text" placeholder="username" />
-                <input className="loginInput" type="password" placeholder="password" />
-                
-                <p className="text">Or login using</p>
-                <div className="login-btn">Login</div>
-                <div className="alt-login">
-                    <div className="login-btn" id="forgot">Forgot Password</div>
-                    <div className="login-btn" id="register" onClick= {() => {handleShow(true);}}>Register New User</div>
+                <div className="loginPage">
+                    <h1>Book-A-Movie</h1>
+                    <input className="loginInput" type="text" placeholder="username" />
+                    <input className="loginInput" type="password" placeholder="password" />
+                    
+                    <p className="text">Or login using</p>
+                    <div className="login-btn">Login</div>
+                    <div className="alt-login">
+                        <div className="login-btn" id="forgot">Forgot Password</div>
+                        <div className="login-btn" id="register" onClick= {() => {handleShow(true);}}>Register New User</div>
+                    </div>
                 </div>
-            </div>
             </form>
         </div>
      );
