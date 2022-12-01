d90d3c39b7f120ed5eb26389578664d32732031f
Stefan Pisic <spisic@uwo.ca>
2022-12-01 17:22:38 -0500

    made modal look nicer

--- react-frontend/src/Components/Modal/Modal.js
+++ react-frontend/src/Components/Modal/Modal.js
@@ -21,13 +21,16 @@ const Modal = props => {
         <div className="modal">
             <div className="modal-content">
                 <div className="modal-header">
-                    <h4 className="modal-title">Movie Name</h4>
+                    <h4 className="modal-title">Morbius</h4>
                 </div>
+                <hr></hr>
                 <div className="modal-body">
-                    Movie Desc
+                Dangerously ill with a rare blood disorder and determined to save others suffering his same fate, Dr. Morbius attempts a desperate gamble. While at first it seems to be a radical success, a darkness inside him is unleashed. Will good override evil -- or will Morbius succumb to his mysterious new urges?
                 </div>
+                <hr></hr>
                 <div className="modal-body2">
                     <label>Please select the amount of tickets you would like to purchase (maximum 15 per purchase). Each ticket is $8.50. Taxes are included. </label> 
+                    <hr></hr>
                     <select name="tickets" id="tickets" onChange={() => {handleCost(true);}}>
                         <option value="option1">1</option>
                         <option value="option2">2</option>
@@ -47,11 +50,13 @@ const Modal = props => {
                     </select>
                 </div>
                 <div className="modal-body3">
-                    <label id="lblName">Total: </label> 
+                    <label id="lblName">Total: $0.00 </label> 
                 </div>
                 <div className="modal-footer">
-                    <button onClick={() => {handleClick(true);}} className="payButton">Pay</button>
-                    <button onClick={() => {handleClick(true);}} className="closeButton">Close</button>
+                    <div className="btn">
+                        <button id="pay" onClick={() => {handleClick(true);}} >Pay</button>
+                        <button id="close" onClick={() => {handleClick(true);}} >Close</button>
+                    </div>
                 </div>
             </div>
         </div>
